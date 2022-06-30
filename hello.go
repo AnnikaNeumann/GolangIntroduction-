98b7b4ac91cd3042ed3405e479112572ad5119f9
Annika83 <annika1983neumann@gmail.com>
2022-06-30 20:53:26 +0100

    First steps into learning Golang

---
+++ hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+	// fmt.Println("Hello, Annika!")
+
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "elliot"
+	// nameFour :="yoshi"
+
+
+	// fmt.Println(nameFour)
+
+	// //ints
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+}
