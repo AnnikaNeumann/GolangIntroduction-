1e0c8b0fa01b5cc4bc9d7120f26fa66ec64f046d
Annika83 <annika1983neumann@gmail.com>
2022-07-05 23:29:32 +0100

    tutorials completed for condictions and how to use functions - Net Ninya YT

--- hello.go
+++ hello.go
@@ -6,28 +6,48 @@ package main
 import (
 	"fmt"
 	// "sort"
+	"math"
 )
 
-func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("value of x is:", x)
-	// 	x++
-	// }
+//Using functions
 
-	// for i := 0; i < 5; i++{
-	// 			fmt.Println("value of i is:", i)
+//name string
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
 
-	// }
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
 
-	names := []string{"Annika", "Elliot", "Mario", "Zelda"}
-
-	 for i := 0; i < len(names); i++{
-				fmt.Println(names[i])
-
+//calling a function for each element inside an array
+func cycleNames(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
 	}
+}
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
 
-	
+// above functions can be called inside the main function down here
+func main() {
+
+	// sayGreeting("Annika")
+	// sayGreeting("Elliot")
+	// sayBye("Dexter")
+
+	//cycles through array of names which are strings and call the function sayGreeting
+	cycleNames([]string{"Captain Picard", "Seven Of Nine", "Data"}, sayGreeting)
+	cycleNames([]string{"Captain Picard", "Seven Of Nine", "Data"}, sayBye)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+
+	fmt.Println(a1, a2)
+	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+
+}
 
 //strings
 // fmt.Println("Hello, Annika!")
@@ -116,17 +136,17 @@ func main() {
 
 // greeting := "Hello there friends"
 
-	// fmt.Println(strings.Contains(greeting, "Hello"))
-	// //replaces Hello with Hi
-	// fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-	// fmt.Println(strings.ToUpper(greeting))
+// fmt.Println(strings.Contains(greeting, "Hello"))
+// //replaces Hello with Hi
+// fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
+// fmt.Println(strings.ToUpper(greeting))
 
-	//searches for the index of a letter in the greeting
-	// fmt.Println(strings.Index(greeting, "ll"))
-	// fmt.Println((strings.Split(greeting, " ")))
+//searches for the index of a letter in the greeting
+// fmt.Println(strings.Index(greeting, "ll"))
+// fmt.Println((strings.Split(greeting, " ")))
 
-	//the original value is unchanged
-	// fmt.Println("original string value=", greeting)
+//the original value is unchanged
+// fmt.Println("original string value=", greeting)
 
 // 	ages := []int{45, 55, 65, 75, 20, 40, 19}
 // 	sort.Ints(ages)
@@ -142,4 +162,63 @@ func main() {
 
 // 	fmt.Println(sort.SearchStrings(names, "Mario"))
 //
-}
+
+//Loops
+// x := 0
+// for x < 5 {
+// 	fmt.Println("value of x is:", x)
+// 	x++
+// }
+
+// for i := 0; i < 5; i++{
+// 			fmt.Println("value of i is:", i)
+
+// }
+
+// names := []string{"Annika", "Elliot", "Mario", "Zelda"}
+
+// // //  for i := 0; i < len(names); i++{
+// // // 			fmt.Println(names[i])
+
+// // // }
+
+// // for index, value := range names{
+// // 	fmt.Printf("the value at index %v is %v \n", index, value)
+// // }
+
+// for _, value := range names{
+// 	fmt.Printf("the value is %v \n", value)
+// }
+// fmt.Println(names)
+
+// conditionals and booleans
+
+// age := 25
+
+// fmt.Println(age <= 50)
+// fmt.Println(age >= 50)
+// fmt.Println(age == 45)
+// fmt.Println(age != 45)
+
+// if age < 30 {
+// 	fmt.Println("age is less than 30")
+// } else if age < 40 {
+// 	fmt.Println("age is less than 40")
+// } else {
+// 	fmt.Println("age is not less than 45")
+// }
+
+// names := []string{"Annika", "Elliot", "Mario", "Zelda"}
+
+// for index, value := range names{
+// 	if index == 1{
+// 		fmt.Println("continuing at pos", index)
+// 		continue
+// 	}
+// 	if index > 2 {
+// 		fmt.Println("breaking at pos", index)
+// 		break
+// 	}
+
+// 	fmt.Printf("the value at pos %v is %v \n", index, value)
+// }
